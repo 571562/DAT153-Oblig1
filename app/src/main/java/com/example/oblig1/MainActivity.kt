3d7f2e189b2e061559bdc13a5b5c8eb29be98ca7
571562 <571562@stud.hvl.no>
2020-01-21 15:35:24 +0100

    laga knappar og activity til dei

--- app/src/main/java/com/example/oblig1/MainActivity.kt
+++ app/src/main/java/com/example/oblig1/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.oblig1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +10,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    fun pressQuiz(view: View) {
+
+        
+
+    }
+
 }
